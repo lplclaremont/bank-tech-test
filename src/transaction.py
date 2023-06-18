7b7ab994984980237d0ed08fc77109228354cf18
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-18 15:37:36 +0100

    passing date test for transaction

--- src/transaction.py
+++ src/transaction.py
@@ -1,6 +1,11 @@
 from datetime import date
 class Transaction:
     def __init__(self, amount):
-        if type(amount) != int:
-            raise Exception("Input must be an integer")
+        if (type(amount) != int) or (amount == 0):
+            raise Exception("Input must be a non zero integer")
         self.amount = amount
+
+    def date(self):
+        today = date.today()
+        return today.strftime("%y/%m/%d")
+    
--- transaction_test.py
+++ transaction_test.py
@@ -1,21 +1,30 @@
 import unittest
+from freezegun import freeze_time
+from datetime import date
 from src.transaction import Transaction
 
 class TestTransaction(unittest.TestCase):
-    transaction = Transaction(100)
+
     def test_valid_amount(self):
         transaction = Transaction(100)
         self.assertEqual(transaction.amount, 100)
     def test_negative_amount(self):
         transaction = Transaction(-100)
         self.assertEqual(transaction.amount, -100)
-    def test_invalid_amount(self):
+    def test_zero_amount_given(self):
+        with self.assertRaises(Exception):
+            transaction = Transaction(0)
+    def test_no_amount_given(self):
         with self.assertRaises(Exception):
             transaction = Transaction()
-
-    def test_date_format(self):
+    def test_invalid_input_type(self):
         with self.assertRaises(Exception):
             transaction = Transaction('five')
+    
+    @freeze_time("2023-06-25")
+    def test_date_format(self):
+        transaction = Transaction(100)
+        self.assertEqual(transaction.date(), '23/06/25')
 
 if __name__ == '__main__':
     unittest.main()
