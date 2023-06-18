ea335248dbca8e779bf7aff629338bca0fe3c098
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-18 14:06:18 +0100

    passing all amount tests

--- src/transaction.py
+++ src/transaction.py
@@ -1,3 +1,6 @@
+from datetime import date
 class Transaction:
     def __init__(self, amount):
+        if type(amount) != int:
+            raise Exception("Input must be an integer")
         self.amount = amount
--- transaction_test.py
+++ transaction_test.py
@@ -3,9 +3,19 @@ from src.transaction import Transaction
 
 class TestTransaction(unittest.TestCase):
     transaction = Transaction(100)
-    def test_amount(self):
+    def test_valid_amount(self):
         transaction = Transaction(100)
         self.assertEqual(transaction.amount, 100)
+    def test_negative_amount(self):
+        transaction = Transaction(-100)
+        self.assertEqual(transaction.amount, -100)
+    def test_invalid_amount(self):
+        with self.assertRaises(Exception):
+            transaction = Transaction()
+
+    def test_date_format(self):
+        with self.assertRaises(Exception):
+            transaction = Transaction('five')
 
 if __name__ == '__main__':
     unittest.main()
