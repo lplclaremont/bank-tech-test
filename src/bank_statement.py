9e4268ea1482ffc567dec3599ce28c8631ee7cd6
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-18 20:03:17 +0100

    refactored view method

--- src/bank_statement.py
+++ src/bank_statement.py
@@ -3,12 +3,13 @@ from .account import Account
 class BankStatement:
     def __init__(self, account):
         self.account = account
+        self.header = "date || credit || debit || balance\n"
     
     def view(self):
-        header = "date || credit || debit || balance\n"
         transactions = self.account.transactions
         transaction_strings = map(self.__format_transaction, transactions)
-        return header + "\n".join(transaction_strings)   
+
+        return self.header + "\n".join(transaction_strings)   
 
     def __format_transaction(self, transaction):
         arr = self.__get_transaction_details_array(transaction)
@@ -21,6 +22,6 @@ class BankStatement:
         if amount > 0:
             return [date, str(amount), "", str(balance)]
         else:
-          return [date, "", str(-amount), str(balance)]
+            return [date, "", str(-amount), str(balance)]
         
     
